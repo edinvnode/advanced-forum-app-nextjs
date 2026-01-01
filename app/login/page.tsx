2596a820c56969b0f112d947982df40f56232542
Edin <edin.vatrich@gmail.com>
2026-01-01 21:47:15 +0100

    Added borders and background

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,21 @@
 export default function Login() {
   return (
-    <div className="flex min-h-screen items-center justify-center border-black rounded">
-      <form className="h-1/2 flex items-center justify-center flex-col bg-yellow-200 p-2 border-black rounded">
-        <p className="mb-2">Login form</p>
+    <div className="flex min-h-screen items-center justify-center border-black rounded bg-blue-600">
+      <form className="h-1/2 flex items-center justify-center flex-col bg-yellow-200 p-2 border border-black rounded">
+        <p className="mb-2 border-black">Login form</p>
         <label className="mb-2">Email:</label>
-        <input className="mb-2 border-black" type="email" />
+        <input
+          className="mb-2 border border-gray-600 bg-green-200"
+          type="email"
+        />
         <label className="mb-2">Password:</label>
-        <input className="mb-2 border-black" type="password" />
-        <button className="mb-2 p-1 border-black bg-red-200 ">Submit</button>
+        <input
+          className="mb-2 border border-gray-600 bg-green-200"
+          type="password"
+        />
+        <button className="mb-2 p-1 border border-gray-600 bg-red-300 w-1/2">
+          Submit
+        </button>
       </form>
     </div>
   );
