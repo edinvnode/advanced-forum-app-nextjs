64683494a0a713831d148dc7b5585bfa1fa9ae88
Edin <edin.vatrich@gmail.com>
2026-01-02 18:43:08 +0100

    Added background

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 export default function Login() {
   return (
-    <div className="flex min-h-screen items-center justify-center border-black rounded bg-blue-600">
+    <div className="flex min-h-screen items-center justify-center border-black rounded bg-purple-300">
       <form className="h-1/2 flex items-center justify-center flex-col bg-yellow-200 p-2 border border-black rounded">
         <p className="mb-2 border-black">Login form</p>
         <label className="mb-2">Email:</label>
