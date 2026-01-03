7ba3102da7c4e384472d0b8f8035b1d9d4a50c9b
Edin <edin.vatrich@gmail.com>
2026-01-03 20:31:28 +0100

    Minor changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,22 @@
 export default function Login() {
   return (
-    <div className="flex min-h-screen items-center justify-center border-black rounded bg-purple-300">
+    <div className="flex min-h-screen items-center justify-center border-black rounded bg-green-400">
       <form className="h-1/2 flex items-center justify-center flex-col bg-yellow-200 p-2 border border-black rounded">
         <p className="mb-2 border-black">Login form</p>
         <label className="mb-2">Email:</label>
         <input
-          className="mb-2 border border-gray-600 bg-green-200"
+          className="mb-2 border border-gray-600 bg-gray-200 w-5/6"
+          placeholder="Email"
           type="email"
         />
         <label className="mb-2">Password:</label>
         <input
-          className="mb-2 border border-gray-600 bg-green-200"
+          className="mb-2 border border-gray-600 bg-gray-200 w-5/6"
+          placeholder="Password"
           type="password"
         />
-        <button className="mb-2 p-1 border border-gray-600 bg-red-300 w-1/2">
-          Submit
+        <button className="mb-2 p-1 border border-gray-600 bg-green-300 w-5/6 mt-2 rounded">
+          Login
         </button>
       </form>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const data = [
+    { id: 1, topicTitle: "New Games", topicAuthor: "admin" },
+    { id: 1, topicTitle: "Old Games", topicAuthor: "admin" },
+    { id: 1, topicTitle: "Best Games", topicAuthor: "admin" },
+    { id: 1, topicTitle: "Clasic Games", topicAuthor: "admin" },
+    { id: 1, topicTitle: "Retro Games", topicAuthor: "admin" },
+  ];
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Hello World</h1>
+    <div className="flex min-h-screen items-center  bg-zinc-50 font-sans dark:bg-black flex-col">
+      {data.map((topic) => (
+        <div className="flex flex-col w-screen text-center border border-black">
+          <h2>{topic.topicTitle}</h2>
+          <span>{topic.topicAuthor}</span>
+        </div>
+      ))}
     </div>
   );
 }
