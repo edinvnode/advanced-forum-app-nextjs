2aaaf6d54d889e8c894b36926e96cd3cef33610e
Edin <edin.vatrich@gmail.com>
2026-01-07 16:28:24 +0100

    Added search function in games page

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -6,6 +6,7 @@ export default function Games() {
   const [games, setGames] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     async function fetchGames() {
@@ -28,17 +29,29 @@ export default function Games() {
     fetchGames();
   }, []);
 
+  const filteredList = games.filter((item) =>
+    item.title.toLowerCase().includes(query.toLowerCase())
+  );
+
   if (loading) return <p className="p-6">Loading...</p>;
   if (error) return <p className="p-6 text-red-500">{error}</p>;
 
   return (
-    <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-4">
-      {games.map((game) => (
-        <div key={game.id} className="border p-3 rounded">
-          <img src={game.thumbnail} alt={game.title} />
-          <h2>{game.title}</h2>
-        </div>
-      ))}
-    </div>
+    <>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search movies..."
+        className="border p-2 mb-4 mt-6 ml-6 w-full max-w-md"
+      />
+      <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-4">
+        {filteredList.map((game) => (
+          <div key={game.id} className="border p-3 rounded">
+            <img src={game.thumbnail} alt={game.title} />
+            <h2>{game.title}</h2>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
