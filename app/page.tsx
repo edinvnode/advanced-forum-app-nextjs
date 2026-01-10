24c7600862e079e7f8eca8e7eb7c518a4aeb597d
Edin <edin.vatrich@gmail.com>
2026-01-10 15:32:56 +0100

    Forum topics modifications

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,76 @@ import Image from "next/image";
 
 export default function Home() {
   const data = [
-    { id: 1, topicTitle: "New Games", topicAuthor: "admin" },
-    { id: 2, topicTitle: "Old Games", topicAuthor: "admin" },
-    { id: 3, topicTitle: "Best Games", topicAuthor: "admin" },
-    { id: 4, topicTitle: "Clasic Games", topicAuthor: "admin" },
-    { id: 5, topicTitle: "Retro Games", topicAuthor: "admin" },
-    { id: 6, topicTitle: "PSX Games", topicAuthor: "admin" },
-    { id: 7, topicTitle: "Xbox Games", topicAuthor: "admin" },
-    { id: 8, topicTitle: "Nintendo Games", topicAuthor: "admin" },
-    { id: 9, topicTitle: "Sega Games", topicAuthor: "admin" },
-    { id: 10, topicTitle: "PS5 Games", topicAuthor: "admin" },
+    {
+      id: 1,
+      topicTitle: "New Games",
+      description: "Talk about latest games on market",
+      topicAuthor: "admin",
+    },
+    {
+      id: 2,
+      topicTitle: "Old Games",
+      description: "Talk about old games you like",
+      topicAuthor: "admin",
+    },
+    {
+      id: 3,
+      topicTitle: "Best Games",
+      description: "What are the best games you played?",
+      topicAuthor: "admin",
+    },
+    {
+      id: 4,
+      topicTitle: "Clasic Games",
+      description: "Know good clasic game? Post about it here.",
+      topicAuthor: "admin",
+    },
+    {
+      id: 5,
+      topicTitle: "Retro Games",
+      description: "Retro games topic.",
+      topicAuthor: "admin",
+    },
+    {
+      id: 6,
+      topicTitle: "PSX Games",
+      description: "Talk about play station games.",
+      topicAuthor: "admin",
+    },
+    {
+      id: 7,
+      topicTitle: "Xbox Games",
+      description: "Xbox games thread.",
+      topicAuthor: "admin",
+    },
+    {
+      id: 8,
+      topicTitle: "Nintendo Games",
+      description: "Like nintendo games? This is the place for you.",
+      topicAuthor: "admin",
+    },
+    {
+      id: 9,
+      topicTitle: "Sega Games",
+      description: "Sega games thread.",
+      topicAuthor: "admin",
+    },
+    {
+      id: 10,
+      topicTitle: "PS5 Games",
+      description: "Tqlk about play station 5 games.",
+      topicAuthor: "admin",
+    },
   ];
 
   return (
     <div className="flex min-h-screen items-center  bg-green-300 font-sans dark:bg-black flex-col">
       {data.map((topic) => (
-        <div className="flex flex-col w-screen text-center border border-black">
-          <h2>{topic.topicTitle}</h2>
+        <div className="flex flex-row w-screen text-center border border-black justify-between pl-5 pr-5">
+          <div>
+            <h2>{topic.topicTitle}</h2>
+            <h3>{topic.description}</h3>
+          </div>
           <span>Created by: {topic.topicAuthor}</span>
         </div>
       ))}
