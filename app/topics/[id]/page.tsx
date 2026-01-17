83d410c699e5b06d59ee922f308fceb2f2101d4d
Edin <edin.vatrich@gmail.com>
2026-01-17 21:37:58 +0100

    Added form to post

--- app/topics/[id]/page.tsx
+++ app/topics/[id]/page.tsx
@@ -20,13 +20,45 @@ export default async function TopicPage({ params }: Props) {
   }
 
   return (
-    <div className="p-10 max-w-3xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4 text-white">{topic.topicTitle}</h1>
-      <p className="text-white mb-2">{topic.topicDescription}</p>
-      <p className="mb-4 text-white">{topic.topicData}</p>
-      <span className="text-sm text-white">
-        Created by: {topic.topicAuthor}
-      </span>
-    </div>
+    <>
+      <div className="p-10 max-w-3xl mx-auto">
+        <h1 className="text-3xl font-bold mb-4 text-white">
+          {topic.topicTitle}
+        </h1>
+        <p className="text-white mb-2">{topic.topicDescription}</p>
+        <p className="mb-4 text-white">{topic.topicData}</p>
+        <span className="text-sm text-white">
+          Created by: {topic.topicAuthor}
+        </span>
+      </div>
+
+      <form className="m-2 flex flex-col" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Post title..."
+          className="border border-black bg-gray-100 m-2"
+          name="postTitle"
+          value={form.topicTitle}
+          onChange={handleChange}
+        />
+
+        <textarea
+          className="bg-gray-100 border border-black m-2"
+          placeholder="Enter your post data here..."
+          cols={50}
+          rows={10}
+          value={form.topicData}
+          name="postData"
+          onChange={handleChange}
+        />
+
+        <button
+          type="submit"
+          className="bg-[#3390d6] text-black border border-black m-2"
+        >
+          Submit thread
+        </button>
+      </form>
+    </>
   );
 }
